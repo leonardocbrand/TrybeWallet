8522715266e8c1df977cbdf2f820626a4231c917
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-07-20 15:59:00 -0300

    fix: test

--- src/tests/Wallet.test.tsx
+++ src/tests/Wallet.test.tsx
@@ -3,46 +3,11 @@ import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
 import { renderWithRedux } from './helpers/renderWith';
 import { Wallet } from '../pages/Wallet';
-import mockUserData from './helpers/mockUserData';
 import mockData from './helpers/mockData';
 
 describe('Testando a página da Wallet "/carteira"', () => {
   beforeEach(() => {
     vi.clearAllMocks();
-  });
-
-  test('Testando os elementos do form', async () => {
-    const state = mockUserData;
-    renderWithRedux(<Wallet />, { initialState: state });
-
-    const value = screen.getByRole('heading', { name: /0\.00/i });
-    const headerCurrency = screen.getByTestId('header-currency-field');
-    const email = screen.getByRole('heading', { name: /email@teste\.com/i });
-    const valueInput = screen.getByTestId('value-input');
-    const descriptionInput = screen.getByTestId('description-input');
-    const submitButton = screen.getByRole('button', { name: /adicionar despesa/i });
-    const selectMethod = screen.getByRole('combobox', {
-      name: /método de pagamento/i,
-    });
-    const selectCurrency = await screen.findByRole('combobox', {
-      name: /moeda/i,
-    });
-    const selectTag = screen.getByRole('combobox', {
-      name: /categoria da despesa/i,
-    });
-
-    expect(value).toBeInTheDocument();
-    expect(headerCurrency).toBeInTheDocument();
-    expect(email).toBeInTheDocument();
-    expect(valueInput).toBeInTheDocument();
-    expect(descriptionInput).toBeInTheDocument();
-    expect(submitButton).toBeInTheDocument();
-    expect(selectMethod).toBeInTheDocument();
-    expect(selectCurrency).toBeInTheDocument();
-    expect(selectTag).toBeInTheDocument();
-    expect(selectMethod).toHaveLength(3);
-    expect(selectCurrency).toHaveLength(15);
-    expect(selectTag).toHaveLength(5);
   });
   test('Testando a tabela de gastos', async () => {
     global.fetch = vi.fn().mockResolvedValue({
